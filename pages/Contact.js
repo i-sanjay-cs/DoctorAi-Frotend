b97468aff4d2e3e008f3b9a99d07abe075cee00d
i-sanjay-cs <sanjay826870@gmail.com>
2024-04-03 21:27:01 +0530

    Changes in the contact web app

--- pages/Contact.js
+++ pages/Contact.js
@@ -10,22 +10,22 @@ const ContactPage = () => {
       <div className="bg-gray-100 p-8 flex flex-col items-center" style={{ minHeight: "100vh" }}>
         <h1 className="text-4xl font-bold mb-8 text-green-800">Contact Page</h1>
         <div className="contact-container" style={{ maxWidth: "600px", margin: "0 auto" }}>
-          <h2>Contact Our Team Members</h2>
-          <div className="team-member" style={{ marginBottom: "20px" }}>
-            <h3 style={{ fontSize: "18px", marginBottom: "5px" }}>Nirmal Avhad</h3>
-            <p style={{ fontSize: "16px" }}>Email: avhadnirmal072@gmail.com</p>
+          <h2 className="text-2xl font-bold mb-4">Contact Our Team Members</h2>
+          <div className="team-member bg-white p-4 rounded-lg shadow-md" style={{ marginBottom: "20px" }}>
+            <h3 style={{ fontSize: "20px", marginBottom: "10px" }}>Nirmal Avhad</h3>
+            <p style={{ fontSize: "18px" }}>Email: avhadnirmal072@gmail.com</p>
           </div>
-          <div className="team-member" style={{ marginBottom: "20px" }}>
-            <h3 style={{ fontSize: "18px", marginBottom: "5px" }}>Anup Muttha</h3>
-            <p style={{ fontSize: "16px" }}>Email: anupmuttha666@gmail.com</p>
+          <div className="team-member bg-white p-4 rounded-lg shadow-md" style={{ marginBottom: "20px" }}>
+            <h3 style={{ fontSize: "20px", marginBottom: "10px" }}>Anup Muttha</h3>
+            <p style={{ fontSize: "18px" }}>Email: anupmuttha666@gmail.com</p>
           </div>
-          <div className="team-member" style={{ marginBottom: "20px" }}>
-            <h3 style={{ fontSize: "18px", marginBottom: "5px" }}>Arshdeep Singh Mathadu</h3>
-            <p style={{ fontSize: "16px" }}>Email: arshdeepsinghmathadu707@gmail.com</p>
+          <div className="team-member bg-white p-4 rounded-lg shadow-md" style={{ marginBottom: "20px" }}>
+            <h3 style={{ fontSize: "20px", marginBottom: "10px" }}>Arshdeep Singh Mathadu</h3>
+            <p style={{ fontSize: "18px" }}>Email: arshdeepsinghmathadu707@gmail.com</p>
           </div>
-          <div className="team-member" style={{ marginBottom: "20px" }}>
-            <h3 style={{ fontSize: "18px", marginBottom: "5px" }}>Sanjay Prajapati</h3>
-            <p style={{ fontSize: "16px" }}>Email: sanjayprajapati82@proton.me</p>
+          <div className="team-member bg-white p-4 rounded-lg shadow-md" style={{ marginBottom: "40px" }}>
+            <h3 style={{ fontSize: "20px", marginBottom: "10px" }}>Sanjay Prajapati</h3>
+            <p style={{ fontSize: "18px" }}>Email: sanjayprajapati82@proton.me</p>
           </div>
         </div>
       </div>
