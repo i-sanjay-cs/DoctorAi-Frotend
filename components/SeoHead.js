50bfd45065117b54140974eea0fa29172c0fea53
i-sanjay-cs <sanjay826870@gmail.com>
2024-05-25 21:24:52 +0530

    Chnages done with respect to the title image

--- components/SeoHead.js
+++ components/SeoHead.js
@@ -1,20 +1,18 @@
 import Head from "next/head";
 import { useRouter } from "next/router";
 
-
 // Default value for some meta data
 const defaultMeta = {
   title: 'DoctorAI',
   siteName: 'DoctorAI',
-  description:
-    'AI healthcare 24/7',
+  description: 'AI healthcare 24/7',
   // change base url of your web (without '/' at the end)
   url: '',
   type: 'website',
   robots: 'follow, index',
   // change with url of your image (recommended dimension = 1.91:1)
-  // used in twitter, facebook, etc. card when link copied in tweet/status 
-  image: '',
+  // used in twitter, facebook, etc. card when link copied in tweet/status
+  image: '/doctorai-title-logo.png', // Updated with new image path
   author: 'Lorem Ipsum'
 };
 
@@ -82,106 +80,18 @@ const SeoHead = (props) => {
           />
         </>
       )}
-      {/* Favicons */}
-      {favicons.map((linkProps) => (
-        <link key={linkProps.href} {...linkProps} />
-      ))}
+      {/* Favicon */}
+      <link rel='icon' href='public/favicon/doctorai-title-logo.png' />
       {/* Windows 8 app icon */}
       <meta name='msapplication-TileColor' content='#F53838' />
       <meta
         name='msapplication-TileImage'
-        content='/favicon/ms-icon-144x144.png'
+        content='/doctorai-title-logo.png'
       />
       {/* Accent color on supported browser */}
       <meta name='theme-color' content='#F53838' />
     </Head>
   );
 };
-
-// Favicons, other icons, and manifest definition
-const favicons = [
-  {
-    rel: 'apple-touch-icon',
-    sizes: '57x57',
-    href: '/favicon/apple-icon-57x57.png',
-  },
-  {
-    rel: 'apple-touch-icon',
-    sizes: '60x60',
-    href: '/favicon/apple-icon-60x60.png',
-  },
-  {
-    rel: 'apple-touch-icon',
-    sizes: '72x72',
-    href: '/favicon/apple-icon-72x72.png',
-  },
-  {
-    rel: 'apple-touch-icon',
-    sizes: '76x76',
-    href: '/favicon/apple-icon-76x76.png',
-  },
-  {
-    rel: 'apple-touch-icon',
-    sizes: '114x114',
-    href: '/favicon/apple-icon-114x114.png',
-  },
-  {
-    rel: 'apple-touch-icon',
-    sizes: '120x120',
-    href: '/favicon/apple-icon-120x120.png',
-  },
-  {
-    rel: 'apple-touch-icon',
-    sizes: '144x144',
-    href: '/favicon/apple-icon-144x144.png',
-  },
-  {
-    rel: 'apple-touch-icon',
-    sizes: '152x152',
-    href: '/favicon/apple-icon-152x152.png',
-  },
-  {
-    rel: 'apple-touch-icon',
-    sizes: '180x180',
-    href: '/favicon/apple-icon-180x180.png',
-  },
-  {
-    rel: 'mask-icon',
-    href: '/favicon/safari-pinned-tab.svg',
-    color: '#F59A9A',
-  },
-  {
-    rel: 'icon',
-    href: '/favicon/favicon.ico',
-  },
-  {
-    rel: 'icon',
-    type: 'image/png',
-    sizes: '16x16',
-    href: '/favicon/favicon-16x16.png',
-  },
-  {
-    rel: 'icon',
-    type: 'image/png',
-    sizes: '32x32',
-    href: '/favicon/favicon-32x32.png',
-  },
-  {
-    rel: 'icon',
-    type: 'image/png',
-    sizes: '96x96',
-    href: '/favicon/favicon-96x96.png',
-  },
-  {
-    rel: 'icon',
-    type: 'image/png',
-    sizes: '192x192',
-    href: '/favicon/android-icon-192x192.png',
-  },
-  {
-    rel: 'manifest',
-    href: '/site.webmanifest',
-  },
-];
 
 export default SeoHead;
